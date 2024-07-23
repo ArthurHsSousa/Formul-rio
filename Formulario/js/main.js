729bd38ae77b399176362f81eef0b65b556b6b28
arthup <arthurhss838@gmail.com>
2024-07-23 16:48:59 -0300

    Comentando o código

--- Formulario/js/main.js
+++ Formulario/js/main.js
@@ -1,23 +1,23 @@
-'use strict';
+'use strict'; //Modo restrirto de programação
 
-const deleteForm = async(endereco) =>{
+const deleteForm = async(endereco) =>{ // Apaga as informações dos input's a cada nova pesquisa do CEP
     document.getElementById('address').value ="";
     document.getElementById('neighborhood').value = "";
     document.getElementById('city').value = "";
     document.getElementById('state').value = "";
 }
 
-const completeForm = async(endereco) =>{
+const completeForm = async(endereco) =>{ // Completa as informações que foram validadas do CEP
     document.getElementById('address').value = endereco.logradouro;
     document.getElementById('neighborhood').value = endereco.bairro;
     document.getElementById('city').value = endereco.localidade;
     document.getElementById('state').value = endereco.uf;
 }
 
-const eNumero = (numero) => /^[0-9]+$/.test(numero);
-const validaCep = (cep) => cep.length == 8 && eNumero(cep);
+const eNumero = (numero) => /^[0-9]+$/.test(numero); // Valida se todos os caractéres da variável (cep) são números
+const validaCep = (cep) => cep.length == 8 && eNumero(cep); // Valida se a variável (cep) tem oito caractéres e se todos são números
 
-const pesquisaCep = async () =>{
+const pesquisaCep = async () =>{ //Pesquisa o CEP
     deleteForm();
     const cep = document.getElementById('cep').value;
     const url =`https://viacep.com.br/ws/${cep}/json/`;
@@ -25,7 +25,7 @@ const pesquisaCep = async () =>{
     if(validaCep(cep)){
         const dados = await fetch(url);
         const endereco = await dados.json();
-        if(endereco.hasOwnProperty('erro')){
+        if(endereco.hasOwnProperty('erro')){ // Essa condição serve para ver se a pesquisa do CEP obteve a propriedade 'erro', caso sim aparece a mensagem de erro, caso não preenche o formulário
             document.getElementById('address').value = "CEP não existe!";
         }else{
             completeForm(endereco);
@@ -36,4 +36,4 @@ const pesquisaCep = async () =>{
     
 }
 
-document.getElementById('cep').addEventListener('focusout', pesquisaCep);
+document.getElementById('cep').addEventListener('focusout', pesquisaCep); // Faz a função quando tira o foco fo input (cep)
